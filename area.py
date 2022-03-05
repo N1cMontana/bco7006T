f59c9af375d7c0322d165572d2d97312de9ed82f
Nico Montana <nicolas.montanabarrios@students.vu.edu.au>
2022-03-05 15:20:22 +1100

    Checking my public connection

--- area.py
+++ area.py
@@ -1,16 +1,3 @@
-#Assessment 1A
-#Write a Python program called area.py.
-#This program will accept two numbers, the width and the height of a rectangle, inputted by the user.
-# Then, the program displays the area of the rectangle on the screen.
+#this is my public Github file
 
-
-print("Do you need to know the area of a rectangle? Let's start")
-
-
-h = float (input("Enter the height of the rectangle in cm  "))
-w = float (input("Enter the weight of the rectangle in cm  "))
-
-area = h * w
-print("The area of the rectangle is", round(area))
-
-print("Bye :)")
+print("Hola")
